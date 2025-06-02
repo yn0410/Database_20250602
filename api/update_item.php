ae0011e6d98b1ec55ffae41de0f61d00b15edc6d
yn <jenny783113@gmail.com>
2025-06-02 16:31:08 +0800

    練習-早餐店網頁 修改品項(未完成)

---
+++ api/update_item.php
@@ -0,0 +1,16 @@
+<?php
+// 全是老師的code
+// 跟"api/add_item.php"差不多
+
+
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+$dsn="mysql:host=localhost;dbname=store;charset=utf8";
+$pdo=new PDO($dsn, 'root', '');
+$sql="UPDATE ";
+$pdo->exec($sql);   
+
+header("Location: ../index.php");
+?>
