cfc97fe6af335b41b34978462d2e279275439469
yn <jenny783113@gmail.com>
2025-06-04 11:29:37 +0800

    練習-會員系統 初始註冊頁面

--- member/reg.php
+++ member/reg.php
@@ -8,7 +8,35 @@
 </head>
 <body>
     <?php include 'header.php'?>
-    <main></main>
+    <main>
+        <h1>會員註冊</h1>
+        <form action="">
+            <div>
+                <label for="name">使用者名稱</label>
+                <input type="text" id="name" name="name" required>
+            </div>
+            <div>
+                <label for="email">電子郵件</label>
+                <input type="text" id="email" name="email" required>
+            </div>
+            <div>
+                <label for="acc">帳號</label>
+                <input type="text" id="acc" name="acc" required>
+            </div>
+            <div>
+                <label for="pw">密碼</label>
+                <input type="password" id="pw" name="pw" required>
+            </div>
+            <div>
+                <label for="birthday">生日</label>
+                <input type="date" id="birthday" name="birthday" required>
+            </div>
+            <div>
+                <button type="submit">註冊</button>
+                <button type="reset">重置</button>
+            </div>
+        </form>
+    </main>
     <?php include 'footer.php'?>
 
 </body>
