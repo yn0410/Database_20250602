7d543d73afa323e3bb22c0888a4e68652e27abf7
yn <jenny783113@gmail.com>
2025-06-04 11:07:47 +0800

    練習-會員系統 初始畫面建構

---
+++ member/header.php
@@ -0,0 +1,10 @@
+<div id='header'>
+    <div class='logo'></div>
+    <div class="nav">
+        <a href="index.php">首頁</a>
+        <a href="member_center.php">會員中心</a>
+        <a href="reg.php">註冊</a>
+        <a href="login.php">登入</a>
+        <a href="logout.php">登出</a>
+    </div>
+</div>
